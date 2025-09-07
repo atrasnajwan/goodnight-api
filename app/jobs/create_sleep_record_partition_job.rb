76ee50523e7d6f13b9675e5e7d70b3e8a5be588e
Atras Najwan <atrasnajwan@gmail.com>
2025-09-07 19:51:21 +0700

    fix add index

--- app/jobs/create_sleep_record_partition_job.rb
+++ app/jobs/create_sleep_record_partition_job.rb
@@ -8,11 +8,6 @@ class CreateSleepRecordPartitionJob < ApplicationJob
 
     unless ActiveRecord::Base.connection.data_source_exists?(table_name)
         SleepRecord.create_partition(start_range: tomorrow, end_range: tomorrow + 1.day)
-
-        # Add indexes to partition table
-        ActiveRecord::Base.connection.execute("CREATE INDEX IF NOT EXISTS sleep_records_#{tomorrow.strftime('%Y_%m_%d')}_user_id_clocked_in_at_idx ON sleep_records_#{tomorrow.strftime('%Y_%m_%d')} USING btree(user_id, clocked_in_at);")
-        ActiveRecord::Base.connection.execute("CREATE INDEX IF NOT EXISTS sleep_records_#{tomorrow.strftime('%Y_%m_%d')}_user_id_clocked_out_at_idx ON sleep_records_#{tomorrow.strftime('%Y_%m_%d')} USING btree(user_id, clocked_out_at);")
-        ActiveRecord::Base.connection.execute("CREATE INDEX IF NOT EXISTS sleep_records_#{tomorrow.strftime('%Y_%m_%d')}_user_id_duration_hours_idx ON sleep_records_#{tomorrow.strftime('%Y_%m_%d')} USING btree(user_id, duration_hours) WHERE clocked_out_at IS NOT NULL;")
     end
 
     Rails.logger.info "[Partition] ensured #{table_name}"
